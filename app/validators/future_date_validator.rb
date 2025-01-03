87ae78693cd93230a94cc017440fc3fbd7a72d7b
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-01-03 01:35:47 -0300

    Até aqui estamos falando sobre uma validação customizada, inclusive utilizando a IA para refatorar e explicar com exemplos o trecho de código.

--- app/validators/future_date_validator.rb
+++ app/validators/future_date_validator.rb
@@ -1,2 +1,9 @@
 class FutureDateValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return if value.blank? # Retorna se o valor for nulo ou vazio
+
+    if value <= Time.zone.now
+      record.errors.add(attribute, :future_date, message: "deve ser uma data futura")
+    end
+  end
 end
