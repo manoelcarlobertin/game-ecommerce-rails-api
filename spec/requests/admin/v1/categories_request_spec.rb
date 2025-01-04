71cf10672af7ed285ee1c2dd4d730b5a9423ebbe
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-01-04 10:58:01 -0300

    Adicionei um contexto onde são criados os dados que queremos que sejam retornados quando chamarmos esse enpoint.

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Admin::V1::Categories", type: :request do
+RSpec.describe "Admin V1 Categories", type: :request do
+  let(:user) { create(:user) }
 
+  context "GET /categories" do
+    let(:url) { "/admin/v1/categories" }
+    let!(:categories) { create_list(:category, 5) }
+  end
 end
+
