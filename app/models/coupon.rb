de4fd04c5f61a8fa727666e6368c125f0d66a7de
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-01-03 21:09:59 -0300

    Apliquei no model e no teste a validação da data do coupon que foi customizada por mim e instalei o active storage para poder gerenciar o upload das imagens dos jogos.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,7 +2,8 @@ class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
   validates :status, presence: true
   validates :discount_value, presence: true, numericality: { greater_than: 0 }
-  validates :due_date, presence: true
+  # simples aplicar uma validação customizada criada por nós =D
+  validates :due_date, presence: true, future_date: true
 
   enum status: { active:1,  inactive: 2 }
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Coupon, type: :model do
   it { is_expected.to validate_presence_of :discount_value }
   it { is_expected.to validate_numericality_of(:discount_value).is_greater_than(0) }
   it { is_expected.to validate_presence_of :due_date }
+
+  it "can't have past due_date é anterior ? " do
+    subject.due_date = 1.day.ago
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+
+  it "is invalid with current due_date igual à data atual? " do
+    subject.due_date = Time.zone.now
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+
+  it "is valid with future date se não está quando é posterior ?" do
+    subject.due_date = Time.zone.now + 1.hour
+    subject.valid?
+    expect(subject.errors.keys).to_not include :due_date # not_included
+  end
 end
