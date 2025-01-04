69ddb05524967e8c0a2f4ec14002f9cc5031b412
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-01-03 22:27:04 -0300

    Observação: Para validações de arquivos (como imagens), considere usar gems como carrierwave-matchers ou realizar testes manuais.Ao comentar image no Factory bot, ou seja, sem presença da image eo teste passou. Essa foi minha dúvida.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,20 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  subject { build(:product) }
-  it { is_expected.to belong_to :productable }
-  it { is_expected.to have_many(:product_categories).dependent(:destroy) }
-  it { is_expected.to have_many(:categories).through(:product_categories) }
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
-  it { is_expected.to validate_presence_of(:description) }
-  it { is_expected.to validate_presence_of(:price) }
-  it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
-  it { is_expected.to validate_presence_of(:image) }
+  describe "Validations" do
+    subject { build(:product) }
 
-  it 'is not valid without an image' do
-    product = build(:product, image: nil) # Cria um produto sem imagem
-    expect(product).not_to be_valid         # Verifica se o produto não é válido
-    expect(product.errors[:image]).to include("can't be blank") # Verifica a mensagem de erro
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+    it { is_expected.to validate_presence_of(:image) }
+
+    context "when image is missing" do
+      let(:product) { build(:product, image: nil) }
+
+      it "is invalid" do
+        expect(product).not_to be_valid
+        expect(product.errors[:image]).to include("não pode ficar em branco")
+      end
+    end
+  end
+
+  describe "Associations" do
+    subject { build(:product) }
+
+    it { is_expected.to belong_to(:productable) }
+    it { is_expected.to have_many(:product_categories).dependent(:destroy) }
+    it { is_expected.to have_many(:categories).through(:product_categories) }
   end
 end
