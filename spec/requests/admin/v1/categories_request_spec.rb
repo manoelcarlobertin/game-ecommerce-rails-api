c0ae908d614a7dc084d0a012b449408213b88fc1
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-01-04 11:05:41 -0300

    Repare que nós utilizamos o body_json, que também criamos no RequestAPI, e buscamos pela chave categories.

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe "Admin V1 Categories", type: :request do
   context "GET /categories" do
     let(:url) { "/admin/v1/categories" }
     let!(:categories) { create_list(:category, 5) }
+
+    # before do
+    #   get url, headers: auth_header(user)
+    # end
+
+    it "returns HTTP 200" do
+      it "returns all Categories" do
+        get url, headers: auth_header(user)
+        expect(body_json['categories']).to contain_exactly *categories.as_json(only: %i(id name))
+      end
+    end
   end
 end
 
